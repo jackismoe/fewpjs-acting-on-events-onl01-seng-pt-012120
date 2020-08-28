cca801022758209799f63d7c5147b1e6a3ca3f09
jackismoe <jacksonmo@me.com>
2020-08-28 11:26:27 -0400

    Attempt Refactor

--- index.js
+++ index.js
@@ -27,3 +27,21 @@ document.addEventListener('keydown', function(e) {
     moveDodgerRight()
   }
 })
+
+
+// const moveDodger = () => {
+//   let leftNumbers = dodger.style.left.replace('px', '')
+//   let left = parseInt(leftNumbers, 10)
+
+//   if (left > 0) {
+//     dodger.style.left = `${left - 1}px`
+//   } else if (left < 360) {
+//     dodger.style.left = `${left + 1}px`
+//   }
+// }
+
+// document.addEventListener('keydown', function(e) {
+//   if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+//     moveDodger()
+//   }
+// })
