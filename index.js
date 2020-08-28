1ec71ce1f9d00f8cf9e1791b39951c0758c4b60b
jackismoe <jacksonmo@me.com>
2020-08-28 11:17:13 -0400

    Resubmit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+let dodger = document.querySelector('body div#game div#dodger')
+
+const moveDodgerLeft = () => {
+  let leftNumbers = dodger.style.left.replace('px', '')
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+const moveDodgerRight = () => {
+  let leftNumbers = dodger.style.left.replace('px', '')
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'ArrowLeft') {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
